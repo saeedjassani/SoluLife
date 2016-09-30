6c460e3fdaea449b5bb264678eeef44abd1ab67c
saeedjassani <saeedjassani@gmail.com>
2016-09-30 15:39:00 +0530

    intitial commit

--- app/src/main/java/com/sapa/solulife/activities/IAmBoredActivity.java
+++ app/src/main/java/com/sapa/solulife/activities/IAmBoredActivity.java
@@ -2,32 +2,40 @@ package com.sapa.solulife.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.*;
 import com.sapa.solulife.R;
+import com.sapa.solulife.adapters.ChapBotRecyclerAdapter;
+import com.sapa.solulife.data.ChatData;
+import com.sapa.solulife.utils.Constant;
 
 public class IAmBoredActivity extends AppCompatActivity {
 
 	RecyclerView recyclerView;
-	RecyclerView.Adapter adapter;
+	ChapBotRecyclerAdapter adapter;
 	RecyclerView.LayoutManager layoutManager;
 	EditText editText;
-	Button  button;
+	Button button;
 	FirebaseApp firebaseApp;
 	FirebaseDatabase firebaseDatabase;
 	DatabaseReference databaseReference;
-	private View.OnClickListener listener =  new View.OnClickListener() {
+
+	private View.OnClickListener listener = new View.OnClickListener() {
 		@Override
 		public void onClick(View view) {
 			switch (view.getId()) {
 				case R.id.chat_send_button:
-
+					String message = editText.getText().toString();
+					if (message.equals("")) return;
+					ChatData chatData = new ChatData(Constant.user_id, message, 1);
+					databaseReference.push().setValue(chatData);
+					editText.setText("");
 					break;
 			}
 		}
@@ -41,10 +49,10 @@ public class IAmBoredActivity extends AppCompatActivity {
 
 		editText = (EditText) findViewById(R.id.chat_send_message);
 		button = (Button) findViewById(R.id.chat_send_button);
-
-
+		recyclerView = (RecyclerView) findViewById(R.id.chat_recycler);
+		adapter = new ChapBotRecyclerAdapter(this);
+		layoutManager = new LinearLayoutManager(this);
 		setUpFirebase();
-
 	}
 
 	private void setUpFirebase() {
@@ -56,5 +64,33 @@ public class IAmBoredActivity extends AppCompatActivity {
 		} else {
 			Toast.makeText(IAmBoredActivity.this, "Not connected", Toast.LENGTH_SHORT).show();
 		}
+
+		databaseReference.addChildEventListener(new ChildEventListener() {
+			@Override
+			public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+				ChatData chatData = dataSnapshot.getValue(ChatData.class);
+				adapter.addMessage(chatData);
+			}
+
+			@Override
+			public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+			}
+
+			@Override
+			public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+			}
+
+			@Override
+			public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+			}
+
+			@Override
+			public void onCancelled(DatabaseError databaseError) {
+
+			}
+		});
 	}
 }
---
+++ app/src/main/java/com/sapa/solulife/data/ChatData.java
@@ -0,0 +1,42 @@
+package com.sapa.solulife.data;
+
+/**
+ * Created by Saeed Jassani on 30-09-2016.
+ */
+
+public class ChatData {
+
+	private int user_id;
+	private String message;
+	private int place;
+
+	public ChatData(int user_id, String message, int place) {
+		this.user_id = user_id;
+		this.message = message;
+		this.place = place;
+	}
+
+	public int getPlace() {
+		return place;
+	}
+
+	public void setPlace(int place) {
+		this.place = place;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
