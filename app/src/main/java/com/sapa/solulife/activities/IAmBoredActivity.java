82d279b967f1714476deb730efc7f73418bffc59
saeedjassani <saeedjassani@gmail.com>
2016-09-30 13:30:05 +0530

    intitial commit

---
+++ app/src/main/java/com/sapa/solulife/activities/IAmBoredActivity.java
@@ -0,0 +1,14 @@
+package com.sapa.solulife.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.sapa.solulife.R;
+
+public class IAmBoredActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_iam_bored);
+	}
+}
---
+++ app/src/main/java/com/sapa/solulife/adapters/ChapBotRecyclerAdapter.java
@@ -0,0 +1,8 @@
+package com.sapa.solulife.adapters;
+
+/**
+ * Created by Saeed Jassani on 30-09-2016.
+ */
+
+public class ChapBotRecyclerAdapter extends Re {
+}
